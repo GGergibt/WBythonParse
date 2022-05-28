30e7d1280d59097d9c72b9ee1c11520574bd0d59
GGergibt <76064583+GGergibt@users.noreply.github.com>
2022-05-28 23:10:55 +0300

    Update prototype_add_products.py

--- prototype_add_products.py
+++ prototype_add_products.py
@@ -4,17 +4,7 @@ from dotenv import load_dotenv
 from woocommerce import API
 
 
-load_dotenv()
 
-consumer_key = os.getenv("CONSUMER_KEY")
-consumer_secret = os.getenv("CONSUMER_SECRET")
-
-wcapi = API(
-    url=URL_OF_YOUR_WP_SITE,
-    consumer_key=consumer_key,
-    consumer_secret=consumer_secret,
-    timeout=50,
-)
 
 
 
@@ -22,8 +12,8 @@ wcapi = API(
 def add_to_wp(pr):
     wcapi = API(
         url="https://4.kpipartners.ru",
-        consumer_key=consumer_key,
-        consumer_secret=consumer_secret,
+        consumer_key=CONSUMER_KEY,
+        consumer_secret=CONSUMER_SECRET,
         timeout=50,
     )
     response = wcapi.post("products", pr)
